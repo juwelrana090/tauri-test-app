602d4c86e4a186dd2af31ab2510e49e036036926
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2025-02-01 08:36:21 +0600

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 };
 
 export default nextConfig;
